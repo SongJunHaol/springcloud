8e7c7576fc13866893c7ddf77b39c5be95779f41
宋军昊 <1870892250@qq.com>
2024-06-21 19:01:19 +0800

    itemscommit

--- item-cart/src/main/java/org/example/CartApplication.java
+++ item-cart/src/main/java/org/example/CartApplication.java
@@ -1,20 +1,14 @@
 package org.example;
 
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @MapperScan("org.example.mapper")
 @SpringBootApplication
-public class ItemApplication {
+public class CartApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ItemApplication.class,args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+        SpringApplication.run(CartApplication.class,args);
     }
 }
---
+++ item-service/src/main/java/org/example/controller/ItemServiceController.java
@@ -0,0 +1,82 @@
+package org.example.controller;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.RandomUtil;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.domain.R;
+import org.example.dto.ItemDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class ItemServiceController {
+
+//    @Autowired
+    public final RestTemplate restTemplate;
+    public final DiscoveryClient discoveryClient;
+
+    @GetMapping("/getUser")
+    @ApiOperation("getUser")
+    public R getUser() {
+        List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("hm-service");
+//        如何集合为空，直接跳出
+        if(CollUtil.isEmpty(serviceInstanceList)){
+            return R.error("二级服务调用错误");
+        }
+//        获取随机数
+        Integer suiji = RandomUtil.randomInt(serviceInstanceList.size());
+        log.info(suiji.toString());
+//        随机获取服务
+        ServiceInstance instance = serviceInstanceList.get(suiji);
+//        获取服务uri
+        URI uri = instance.getUri();
+
+
+        ResponseEntity<ItemDTO> exchange = restTemplate.exchange(
+                 uri+"/items/317578",
+                HttpMethod.GET,
+                null,
+                ItemDTO.class
+        );
+        if(!exchange.getStatusCode().is2xxSuccessful()){
+            return R.error("查询失败");
+        }
+        ItemDTO itemDTOList = exchange.getBody();
+
+        Router router = new Router();
+        router.setName("Admin");
+        router.setJiage("10");
+        router.setItemDTO(itemDTOList);
+        return R.ok(router);
+//        return R.ok("你好");
+    }
+
+}
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+class Router {
+    public String name;
+    public String jiage;
+    public ItemDTO itemDTO;
+
+}
